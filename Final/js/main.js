d291a480efdecd85ecfd9ca118708a0332bc4f8f
UnRenault <fackinsheed@gmail.com>
2018-04-10 20:25:34 +0300

    закончил проэкт

---
+++ Final/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.owl-carousel.main-slider').owlCarousel({
+      items:1,
+      margin:10,
+      loop: true,
+      autoHeight:true
+  });
+  $('.owl-carousel.blog-slider').owlCarousel({
+      items:3,
+      margin:30,
+      loop: true,
+      autoHeight:true
+  });
+});
